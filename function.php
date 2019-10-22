d228c112023b0f05c75d1e0541baeb4afec7ce5b
Nyarlathotep1349 <55247057+Nyarlathotep1349@users.noreply.github.com>
2019-10-22 18:12:22 +0300

    Add files via upload

---
+++ function.php
@@ -0,0 +1,27 @@
+﻿<?php
+function clean($postValue)
+{
+    $value = htmlspecialchars(trim($postValue));
+    return $value;
+}
+
+function jsonResponse($success, $errors = NULL)
+{
+    echo json_encode([
+        'success' => $success,
+        'errors' => $errors
+    ]);
+    exit();
+}
+
+//$xml - объект SimpleXMLElement
+function saveXml($xml)
+{
+    $save = $xml->asXML('db/xmlDataStorage.xml');
+    if ($save){
+        jsonResponse(true);
+    }else{
+        $errors[]['info'] = 'Ошибка. Не удалось сохранить данные.';
+        jsonResponse(false, $errors);
+    }
+}
